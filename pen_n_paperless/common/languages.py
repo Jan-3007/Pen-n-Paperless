4c47f3231bdc909003c3c5b7e7b37d434d1ac50a
Jan-3007 <159416763+Jan-3007@users.noreply.github.com>
2026-01-21 22:10:28 +0100

    Create languages.py

---
+++ pen_n_paperless/common/languages.py
@@ -0,0 +1,19 @@
+# Python module imports
+from enum import Enum, auto
+
+# internal module imports
+
+
+
+class Language(Enum):
+    """Enum containing all supported languages
+
+    For a full list of language codes see e.g.:
+    https://www.w3schools.com/tags/ref_language_codes.asp
+    """
+    ENGLISH = "en"
+    GERMAN = "de"
+
+
+
+

71b55e02dac3cc05c2d4fb17784bedf6089714a3
Jan-3007 <159416763+Jan-3007@users.noreply.github.com>
2026-01-21 22:12:16 +0100

    Add app startup function

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from pen_n_paperless import create_app
+
+
+
 def main():
-    print("Hello from pen-n-paperless!")
+    print("Starting up pen-n-paperless!")
+    
+    app = create_app()
+
+    app.run(
+        debug=True,
+        host='127.0.0.1', port=5001     # app can be accessed using 'http://127.0.0.1:5001' or 'localhost:5000' only on your machine (except the network configuration is different)
+#        host='0.0.0.0', port=5000      # app can be accessed from any device on your network using 'http://<your-devices-IP>:5000'
+        )
 
 
 if __name__ == "__main__":

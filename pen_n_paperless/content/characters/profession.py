334889ba02c4a76c44bff2d8e8d2f0207071eeb7
Jan-3007 <159416763+Jan-3007@users.noreply.github.com>
2026-01-21 22:15:09 +0100

    Implement Profession class based on character property interface

---
+++ pen_n_paperless/content/characters/profession.py
@@ -0,0 +1,175 @@
+
+
+
+# Python module imports
+import logging
+
+# internal imports
+from  ..character_properties_itf import CharacterPropertiesInterface
+
+from ...common.keys import Professions as key
+from ...common.keys import Generic, Statistics, Specializations
+
+from ...common.languages import Language
+from ...config.general import GeneralConfig
+
+
+
+class Profession(CharacterPropertiesInterface):
+
+
+    # profession properties
+    _professions = {
+        key.BARD: {
+            Generic.NAME: {
+                Language.ENGLISH: "Bard",
+                Language.GERMAN: "Barde"
+                },
+            Generic.DESCRIPTION: {
+                Language.ENGLISH: "A charismatic performer and storyteller, skilled in music and persuasion."
+                },
+            Specializations.SPECIALIZATION: [
+                Specializations.SPY, 
+                Specializations.ARTIST
+                ],
+            Statistics.HP: 4,
+        },
+        key.THIEF: {
+            Generic.NAME: {
+                Language.ENGLISH: "Thief",
+                Language.GERMAN: "Dieb"
+                },
+            Generic.DESCRIPTION: {
+                Language.ENGLISH: "A cunning and stealthy individual, adept at sneaking and lockpicking."
+                },
+            Specializations.SPECIALIZATION: [
+                Specializations.PICKPOCKET, 
+                Specializations.ASSASSIN
+                ],
+            Statistics.HP: 6,
+        },
+        key.HERBALIST: {
+            Generic.NAME: {
+                Language.ENGLISH: "Herbalist", 
+                Language.GERMAN: "Kräuterkundiger"
+                },
+            Generic.DESCRIPTION: {
+                Language.ENGLISH: "A knowledgeable healer and potion maker, skilled in the use of herbs and natural remedies."
+                },
+            Specializations.SPECIALIZATION: [
+                Specializations.CHEMIST, 
+                Specializations.HEALER
+                ],
+            Statistics.HP: 6,
+        },
+        key.WARRIOR: {
+            Generic.NAME: {
+                Language.ENGLISH: "Warrior", 
+                Language.GERMAN: "Krieger"
+                },
+            Generic.DESCRIPTION: {
+                Language.ENGLISH: "A strong and skilled fighter, trained in the art of combat."
+                },
+            Specializations.SPECIALIZATION: [
+                Specializations.BERSERKER, 
+                Specializations.BOXER, 
+                Specializations.SOLDIER, 
+                Specializations.GUARD
+                ],
+            Statistics.HP: 10,
+        },
+        key.RANGER: {
+            Generic.NAME: {
+                Language.ENGLISH: "Ranger", 
+                Language.GERMAN: "Waldläufer"
+                },
+            Generic.DESCRIPTION: {
+                Language.ENGLISH: "A skilled tracker and hunter, adept at surviving in the wilderness."
+                },
+            Specializations.SPECIALIZATION: [
+                Specializations.SCOUT, 
+                Specializations.BEASTMASTER
+                ],
+            Statistics.HP: 8,
+        },
+        key.SCIENTIST: {
+            Generic.NAME: {
+                Language.ENGLISH: "Scientist", 
+                Language.GERMAN: "Wissenschaftler"
+                },
+            Generic.DESCRIPTION: {
+                Language.ENGLISH: "An intellectual and curious individual, skilled in the pursuit of knowledge and discovery."
+                },
+            Specializations.SPECIALIZATION: [
+                Specializations.ALCHEMIST, 
+                Specializations.GEOLOGIST, 
+                Specializations.MATHEMATICIAN, 
+                Specializations.PHARMACIST
+                ],
+            Statistics.HP: 3,
+        },
+    }
+
+
+    
+    @classmethod
+    def get_all(cls) -> list:
+        """
+        Returns a list of all available professions
+        
+        :param cls: Description
+        :return: List of all available professions as keys, empty list if no professions exist
+        :rtype: list[str]
+        """
+
+        return list(cls._professions.keys())
+
+
+
+    @classmethod
+    def get_properties(cls, key) -> dict:
+        """
+        Returns the properties of a profession.
+        
+        :param cls: Description
+        :param key: A profession key. Can be obtained by calling get_all()
+        :return: Properties of the profession, empty dict if key does not exist.
+        :rtype: dict[Any, Any]
+        """
+        
+        return cls._professions.get(key, {})
+
+
+
+    @classmethod
+    def get_all_names(cls) -> dict:
+        profession_dict = {}
+
+        for p_key in Profession.get_all():
+            properties = Profession.get_properties(p_key)
+            name_dict = properties.get(Generic.NAME, {})
+
+            if name_dict == {}:
+                logging.warning(f"get_all_names(): get_all() returned the key {p_key}, but get_properties() did not include a value for key{Generic.NAME}")
+                profession_dict[Generic.NONE] = "No profession found"
+            else:
+                # add entry to dict with profession key and its display name
+                profession_dict[p_key] = name_dict.get(GeneralConfig.language(), "")
+
+        return profession_dict
+
+
+
+    # @classmethod
+    # def get_specializations(cls, profession_key) -> dict:
+    #     """Returns all specializations available for a specific profession"""
+    #     return cls._professions.get(profession_key, {}).get(Strings.specialization.value, [])
+
+    # @classmethod
+    # def name(self, profession_name) -> str:
+    #     """returns the language-dependent name of the profession"""
+    #     return self._professions[profession_name][Strings.names.value][rules.language]
+
+
+    
+

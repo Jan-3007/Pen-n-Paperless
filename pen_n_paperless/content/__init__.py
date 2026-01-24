8f8e848a359780591e078b61796371a65c6cbb9b
Jan-3007 <159416763+Jan-3007@users.noreply.github.com>
2026-01-24 19:59:07 +0100

    Fix import issue in content submodule

--- pen_n_paperless/content/__init__.py
+++ pen_n_paperless/content/__init__.py
@@ -1,7 +1,7 @@
 
-from characters.tribe import Tribe
-from characters.profession import Profession
-from characters.specialization import Specialization
+from .characters.tribe import Tribe
+from .characters.profession import Profession
+from .characters.specialization import Specialization
 
 
 # define which modules get to be automatically imported when 'from content import *' is called
